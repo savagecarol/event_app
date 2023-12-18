f37aad2119252d2378693e04391732ef53c56810
Piyush Khurana <piyushkhurana@Piyushs-MacBook-Air.local>
2023-12-18 21:34:45 +0530

    Updated the color constants

--- lib/utils/colors_constants.dart
+++ lib/utils/colors_constants.dart
@@ -1,10 +1,12 @@
-import 'package:flutter/material.dart';
 
 /*
  *
  * APP COLOR
  * 
 */
+
+
+import 'package:flutter/material.dart';
 
 class ColorConstants {
   static  MaterialColor appRed = Color.fromRGBO(207, 21, 60, 1) as MaterialColor;
@@ -19,4 +21,15 @@ class ColorConstants {
   static  Color grey1 = Colors.grey.shade400;
   static  Color fontDarkColor = Colors.grey.shade800;
   static const Color amberColor = Colors.amber;
+
+  static const Color dodgerBlue =  Color.fromRGBO(52, 152, 219, 1); // Main Background color
+  static const Color alizarianCrimson =  Color.fromRGBO(231, 76, 60, 1); // Header and Accent color
+  static const Color midnightColor =  Color.fromRGBO(44, 62, 80, 1); //Text color
+  static const Color emerald =  Color.fromRGBO(39, 174, 96, 1); // Button color
+  static const Color silver =  Color.fromRGBO(236, 240, 241, 1); // Secondary Background Color
+  static const Color orange =  Color.fromRGBO(243, 156, 18, 1); // Highlight Color
+  static const Color silverSand = Color.fromRGBO(189, 195, 199, 1); // Border color
+  static const Color amethyst =  Color.fromRGBO(155, 89, 182, 1); //Link color
+  static const Color turquoise = Color.fromRGBO(46, 204, 113, 1); // Success/Positive Feedback Color
+  static const Color alizarinCrimson =  Color.fromRGBO(231, 76, 60, 1); //Error/ Negative Feedback color
 }
